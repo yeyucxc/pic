ace56e392366d24e291863839e1bfcdc3d914a7f
xcchen <55827687+yeyucxc@users.noreply.github.com>
2020-04-03 13:40:12 +0800

    Update autoload.js

--- live2d_load_own/autoload.js
+++ live2d_load_own/autoload.js
@@ -1,6 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径，记得修改哦
-const live2d_path = "http://yeyucxc.top:4545/live2d_load_own/";
-const tips_path = "http://yeyucxc.top:4545/live2d_load_own/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/yeyucxc/pic/live2d_load_own/";
+const tips_path = "https://cdn.jsdelivr.net/gh/yeyucxc/pic/live2d_load_own/";
 //const live2d_path = "";
 //const live2d_path = "/live2d-widget/";
 
@@ -34,7 +34,7 @@ function loadExternalResource(url, type) {
 	]).then(() => {
 		initWidget({
 			waifuPath: tips_path + "waifu-tips.min.json",
-			apiPath: "http://yeyucxc.top:4545/live2d_api/",
+			apiPath: "https://cdn.jsdelivr.net/gh/yeyucxc/pic/live2d_api/",
 			//cdnPath: "https://你的CDN域名/live2d_api/"从本地加载此项不用管
 		});
 	});
