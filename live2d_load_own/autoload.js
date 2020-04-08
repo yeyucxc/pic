a2743a8418fa040c93b3af334ff39c3e68e1fd5c
yeyucxc <yeyucxc@163.com>
2020-04-08 12:38:05 +0800

    ds

--- live2d_load_own/autoload.js
+++ live2d_load_own/autoload.js
@@ -1,6 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径，记得修改哦
-const live2d_path = "http://yeyucxc.top:4545/live2d_load_own/";
-const tips_path = "http://yeyucxc.top:4545/live2d_load_own/";
+const live2d_path = "https://yeyucxc.top/wp-content/themes/Sakura/live2d_load_own/";
+const tips_path = "https://yeyucxc.top/wp-content/themes/Sakura/live2d_load_own/";
 //const live2d_path = "";
 //const live2d_path = "/live2d-widget/";
 
@@ -34,8 +34,8 @@ function loadExternalResource(url, type) {
 	]).then(() => {
 		initWidget({
 			waifuPath: tips_path + "waifu-tips.min.json",
-			apiPath: "http://yeyucxc.top:4545/live2d_api/",
-			//cdnPath: "https://你的CDN域名/live2d_api/"从本地加载此项不用管
+			apiPath: "https://yeyucxc.top/wp-content/themes/Sakura/live2d_api/",
+			cdnPath: "https://yeyucxc.top/wp-content/themes/Sakura/live2d_api/"从本地加载此项不用管
 		});
 	});
 console.log(`
